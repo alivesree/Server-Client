bf4bc2c7741ef3f3461c2683840a1f5ffd52c9d7
Sreehari Kalavoor <sreehari@kiebot.com>
2021-06-17 10:21:52 +0400

    mqtt and udp implemented

--- Client/frmClient.Designer.cs
+++ Client/frmClient.Designer.cs
@@ -36,19 +36,28 @@
             this.btnConnect = new System.Windows.Forms.Button();
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.brnSend = new System.Windows.Forms.Button();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.rdbTCP = new System.Windows.Forms.RadioButton();
+            this.rdbUDP = new System.Windows.Forms.RadioButton();
+            this.rdbMQTT = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTopic = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Location = new System.Drawing.Point(14, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "host";
+            this.label1.Text = "IP";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(197, 41);
+            this.label2.Location = new System.Drawing.Point(176, 34);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 1;
@@ -56,29 +65,31 @@
             // 
             // txtHost
             // 
-            this.txtHost.Location = new System.Drawing.Point(77, 34);
+            this.txtHost.Location = new System.Drawing.Point(34, 31);
             this.txtHost.Name = "txtHost";
             this.txtHost.Size = new System.Drawing.Size(100, 20);
             this.txtHost.TabIndex = 2;
+            this.txtHost.Text = "192.168.1.192";
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(238, 38);
+            this.txtPort.Location = new System.Drawing.Point(207, 31);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.Size = new System.Drawing.Size(48, 20);
             this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "8000";
             // 
             // txtStatus
             // 
-            this.txtStatus.Location = new System.Drawing.Point(49, 257);
+            this.txtStatus.Location = new System.Drawing.Point(12, 300);
             this.txtStatus.Multiline = true;
             this.txtStatus.Name = "txtStatus";
-            this.txtStatus.Size = new System.Drawing.Size(357, 189);
+            this.txtStatus.Size = new System.Drawing.Size(608, 103);
             this.txtStatus.TabIndex = 4;
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(371, 38);
+            this.btnConnect.Location = new System.Drawing.Point(301, 30);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(75, 23);
             this.btnConnect.TabIndex = 6;
@@ -88,15 +99,15 @@
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(38, 76);
+            this.txtMessage.Location = new System.Drawing.Point(12, 148);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(357, 78);
+            this.txtMessage.Size = new System.Drawing.Size(608, 104);
             this.txtMessage.TabIndex = 7;
             // 
             // brnSend
             // 
-            this.brnSend.Location = new System.Drawing.Point(49, 172);
+            this.brnSend.Location = new System.Drawing.Point(545, 258);
             this.brnSend.Name = "brnSend";
             this.brnSend.Size = new System.Drawing.Size(75, 23);
             this.brnSend.TabIndex = 8;
@@ -104,11 +115,97 @@
             this.brnSend.UseVisualStyleBackColor = true;
             this.brnSend.Click += new System.EventHandler(this.brnSend_Click);
             // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(12, 409);
+            this.txtHex.Multiline = true;
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(608, 109);
+            this.txtHex.TabIndex = 9;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(403, 30);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 10;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // rdbTCP
+            // 
+            this.rdbTCP.AutoSize = true;
+            this.rdbTCP.Checked = true;
+            this.rdbTCP.Location = new System.Drawing.Point(47, 29);
+            this.rdbTCP.Name = "rdbTCP";
+            this.rdbTCP.Size = new System.Drawing.Size(46, 17);
+            this.rdbTCP.TabIndex = 11;
+            this.rdbTCP.TabStop = true;
+            this.rdbTCP.Text = "TCP";
+            this.rdbTCP.UseVisualStyleBackColor = true;
+            // 
+            // rdbUDP
+            // 
+            this.rdbUDP.AutoSize = true;
+            this.rdbUDP.Location = new System.Drawing.Point(167, 29);
+            this.rdbUDP.Name = "rdbUDP";
+            this.rdbUDP.Size = new System.Drawing.Size(48, 17);
+            this.rdbUDP.TabIndex = 12;
+            this.rdbUDP.Text = "UDP";
+            this.rdbUDP.UseVisualStyleBackColor = true;
+            this.rdbUDP.CheckedChanged += new System.EventHandler(this.rdbUDP_CheckedChanged);
+            // 
+            // rdbMQTT
+            // 
+            this.rdbMQTT.AutoSize = true;
+            this.rdbMQTT.Location = new System.Drawing.Point(297, 29);
+            this.rdbMQTT.Name = "rdbMQTT";
+            this.rdbMQTT.Size = new System.Drawing.Size(56, 17);
+            this.rdbMQTT.TabIndex = 13;
+            this.rdbMQTT.TabStop = true;
+            this.rdbMQTT.Text = "MQTT";
+            this.rdbMQTT.UseVisualStyleBackColor = true;
+            this.rdbMQTT.CheckedChanged += new System.EventHandler(this.rdbMQTT_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtTopic);
+            this.groupBox1.Controls.Add(this.rdbTCP);
+            this.groupBox1.Controls.Add(this.rdbMQTT);
+            this.groupBox1.Controls.Add(this.rdbUDP);
+            this.groupBox1.Location = new System.Drawing.Point(12, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(608, 71);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Protocol";
+            // 
+            // txtTopic
+            // 
+            this.txtTopic.Enabled = false;
+            this.txtTopic.Location = new System.Drawing.Point(429, 27);
+            this.txtTopic.Name = "txtTopic";
+            this.txtTopic.Size = new System.Drawing.Size(155, 20);
+            this.txtTopic.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(388, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Topic";
+            // 
             // frmClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 458);
+            this.ClientSize = new System.Drawing.Size(632, 532);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.txtHex);
             this.Controls.Add(this.brnSend);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.btnConnect);
@@ -120,6 +217,8 @@
             this.Name = "frmClient";
             this.Text = "Client";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,6 +234,14 @@
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Button brnSend;
+        private System.Windows.Forms.TextBox txtHex;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.RadioButton rdbTCP;
+        private System.Windows.Forms.RadioButton rdbUDP;
+        private System.Windows.Forms.RadioButton rdbMQTT;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTopic;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Client/frmClient.cs
+++ Client/frmClient.cs
@@ -13,6 +13,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace client
 {
@@ -23,10 +25,50 @@ namespace client
             InitializeComponent();
         }
         SimpleTcpClient client;
+        MqttClient Mqclient;
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            btnConnect.Enabled = false;
-            client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+            try
+            {
+
+                if (rdbMQTT.Checked)
+                {
+                    Mqclient = new MqttClient(txtHost.Text);
+                    Mqclient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                    Mqclient.Subscribe(new string[] { txtTopic.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                    Mqclient.Connect("SDH-DebugTool");
+                }
+                else
+                {
+                    btnConnect.Enabled = false;
+                    client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+                }
+            }
+            catch (Exception ex)
+            {
+                btnConnect.Enabled = true;
+                MessageBox.Show(ex.Message);
+               // txtStatus.Text += ex.Message;
+            }
+
+        }
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnConnect.Enabled = true;
+                if (Mqclient != null && Mqclient.IsConnected)
+                {
+                    Mqclient.Disconnect();
+                }
+
+                client.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -40,13 +82,61 @@ namespace client
         {
             txtStatus.Invoke((MethodInvoker)delegate ()
             {
-                txtStatus.Text += e.MessageString;
+                txtHex.Text += string.Format("{0} : {1}{2}", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), BitConverter.ToString(e.Data).Replace("-", ""), Environment.NewLine);
+                txtStatus.Text += string.Format("{0} : {1}{2}", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), e.MessageString, Environment.NewLine);
             });
         }
 
         private void brnSend_Click(object sender, EventArgs e)
         {
-            client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(3));
+            try
+            {
+
+                if (rdbMQTT.Checked && Mqclient.IsConnected)
+                {
+                    Mqclient.Publish(txtTopic.Text, Encoding.ASCII.GetBytes(txtMessage.Text)); // Publishing message from client.
+                }
+                else if (rdbTCP.Checked)
+                {
+                    client.WriteLine(txtMessage.Text);
+                }
+                else if (rdbUDP.Checked)
+                {
+                    IPEndPoint RemoteEndPoint = new IPEndPoint(IPAddress.Parse(txtHost.Text), Convert.ToInt32(txtPort.Text));
+                    Socket server = new Socket(AddressFamily.InterNetwork,
+                                               SocketType.Dgram, ProtocolType.Udp);
+                    string msg = (char)0x0A + txtMessage.Text + (char)0x0D;
+                    var data = Encoding.ASCII.GetBytes(msg);
+                    server.SendTo(data, data.Length, SocketFlags.None, RemoteEndPoint);
+                }
+                else
+                {
+                    MessageBox.Show("Not Connected!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            txtHex.Text += string.Format("{0} : {1}{2}", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), BitConverter.ToString(e.Message).Replace("-", ""), Environment.NewLine);
+            txtStatus.Text += string.Format("{0} : {1}{2}", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), Encoding.ASCII.GetString(e.Message), Environment.NewLine);
+        }
+
+        private void rdbMQTT_CheckedChanged(object sender, EventArgs e)
+        {
+            txtTopic.Clear();
+            txtTopic.Enabled = rdbMQTT.Checked;
+            btnConnect.Enabled = true;
+        }
+
+        private void rdbUDP_CheckedChanged(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = !rdbUDP.Checked;
         }
     }
 
